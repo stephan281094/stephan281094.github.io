0c60c8dd111ff808eb8ebfb7489b8250d3c4ad18
stephan281094 <stephan281094@gmail.com>
2015-02-02 09:53:38 +0100

    Made start with contact popup

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,18 +1,8 @@
 $(document).ready(function() {
-	var screenHeight = $(window).height();
+	var contactLink = $('#hero a');
+	var contact 	= $('#contact');
 
-	$(window).scroll(function() {
-		var scrollTop = $(window).scrollTop();
-		var selector  = $('#hero .wrap');
-		var opacity   = (screenHeight - scrollTop) * (1 / screenHeight);
-
-		selector.css({
-			"-webkit-transform": "translate(0, " + scrollTop / 2 + "px)",
-			"-moz-transform": "translate(0, " + scrollTop / 2 + "px)",
-			"transform": "translate(0, " + scrollTop / 2 + "px)",
-			"opacity": opacity
-		});
-
-		console.log(opacity);
+	contactLink.click(function() {
+		contact.addClass('show');
 	});
 });
