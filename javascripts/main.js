2c34c924f731b2a41220266612a52c2d0007eac5
stephan281094 <stephan281094@gmail.com>
2015-02-03 00:00:47 +0100

    Added contact form

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
-	var contactLink = $('#hero a');
-	var contact 	= $('#contact');
+	var body = $('body');
+	var modalOpen = 'modal-open';
 
-	contactLink.click(function() {
-		contact.addClass('show');
+	$('[data-toggle]').click(function() {
+		$('[data-modal="' + $(this).attr('data-toggle') + '"]').toggleClass('show');
+		body.toggleClass(modalOpen);
+	});
+
+	$(this).keyup(function(event){
+		if (event.keyCode === 27 && body.hasClass(modalOpen)) {
+			$('[data-modal]').removeClass('show');
+			body.removeClass(modalOpen);
+		}
 	});
 });
