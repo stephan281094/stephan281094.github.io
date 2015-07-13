5380d09e020f316ca4e3a81a61d510570b79608a
stephan281094 <stephan281094@gmail.com>
2015-07-13 20:35:35 +0200

    Remove jquery dependency and improve JS

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,16 +1,65 @@
-$(document).ready(function() {
-	var body = $('body');
-	var modalOpen = 'modal-open';
+(function() {
+	var shown = false;
+	var defaultOptions = {
+		element: '[data-modal]'
+	}
 
-	$('[data-toggle]').click(function() {
-		$('[data-modal="' + $(this).attr('data-toggle') + '"]').toggleClass('show');
-		body.toggleClass(modalOpen);
-	});
+	var api = {
 
-	$(this).keyup(function(event){
-		if (event.keyCode === 27 && body.hasClass(modalOpen)) {
-			$('[data-modal]').removeClass('show');
-			body.removeClass(modalOpen);
+		open: function(modalName, callback) {
+			if (shown) return;
+
+			var modal = document.querySelector('.modal-' + modalName);
+			modal.classList.add('modal-open');
+			shown = true;
+		},
+
+		close: function() {
+			if (!shown) return;
+
+			var modals = document.querySelectorAll('.modal');
+			for (var i = modals.length - 1; i >= 0; i--) {
+				modals[i].classList.remove('modal-open');
+			};
+
+			shown = false;
+		},
+
+		listen: function listen(element) {
+			if (!element) element = defaultOptions.element
+
+			if (typeof element === 'string') {
+                var elements = document.querySelectorAll(element),
+                    i = elements.length;
+
+                while (i--) {
+                    listen(elements[i]);
+                }
+
+                return;
+            }
+
+			element.addEventListener('click', function(event) {
+				event.stopPropagation();
+
+				if (shown) {
+					api.close();
+				} else {
+					api.open(element.dataset.modal);
+				}
+			})
+
+			return this;
 		}
+	}
+
+	document.addEventListener('keyup', function(event) {
+		if (event.keyCode === 27) api.close();
 	});
-});
+
+	this.Modal = api;
+})();
+
+window.onload = function() {
+	Modal.listen();
+}
