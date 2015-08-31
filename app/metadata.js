89aaa055608bd9d4a659f792722f0f28c6b43f5a
stephan281094 <stephan281094@gmail.com>
2015-08-31 22:10:21 +0200

    Move old files back So we have a website to show :P

--- app/metadata.js
+++ app/metadata.js
@@ -1,5 +1,5 @@
 PostList = [
   {id: 1, title: 'My first blog post', template: 'first', date: '30 aug 2015'},
-  {id: 2, title: 'My second blog post', template: 'second', date: '31 aug 2015'},
+  {id: 2, title: 'Static pages with Meteor', template: 'meteor-build-client', date: '31 aug 2015'},
   {id: 3, title: 'My third blog post', template: 'third', date: '1 sep 2015'}
 ];
