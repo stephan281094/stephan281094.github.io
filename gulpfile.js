b133ba53fc19ec2b144c54786cb8f7f2c629e1a4
stephan281094 <stephan281094@gmail.com>
2015-11-08 18:51:26 +0100

    Convert indents with 2 spaces

--- gulpfile.js
+++ gulpfile.js
@@ -6,27 +6,27 @@ var minifyjs   = require('gulp-uglify')
 var autoprefix = require('gulp-autoprefixer')
 
 var paths = {
-    scss: 'client/stylesheets/**/*.scss',
-    js: 'client/javascripts/**/*.js'
+  scss: 'client/stylesheets/**/*.scss',
+  js: 'client/javascripts/**/*.js'
 }
 
 gulp.task('scss', function() {
-    gulp.src(paths.scss)
-        .pipe(sass({errLogToConsole: true}))
-        .pipe(autoprefix('last 15 versions'))
-        .pipe(concat('complete.min.css'))
-        .pipe(minifycss())
-        .pipe(gulp.dest('build/'))
+  gulp.src(paths.scss)
+    .pipe(sass({errLogToConsole: true}))
+    .pipe(autoprefix('last 15 versions'))
+    .pipe(concat('complete.min.css'))
+    .pipe(minifycss())
+    .pipe(gulp.dest('build/'))
 })
 
 gulp.task('js', function() {
-    gulp.src(paths.js)
-        .pipe(concat('complete.min.js'))
-        .pipe(minifyjs())
-        .pipe(gulp.dest('build/'))
+  gulp.src(paths.js)
+    .pipe(concat('complete.min.js'))
+    .pipe(minifyjs())
+    .pipe(gulp.dest('build/'))
 })
 
 gulp.task('default', ['scss', 'js'], function() {
-    gulp.watch(paths.scss, ['scss'])
-    gulp.watch(paths.js, ['js'])
+  gulp.watch(paths.scss, ['scss'])
+  gulp.watch(paths.js, ['js'])
 })
