14a3f42f79c8d5dd3d2ae17e24fc528c5304dec2
stephan281094 <stephan281094@gmail.com>
2015-11-11 09:39:20 +0100

    Add sweet animations

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -1,2 +1,16 @@
 window.fitText(document.querySelector('h1'));
 window.fitText(document.querySelector('span'), 2);
+
+window.onload = function() {
+  var els = ['h1', 'span', 'a.button'],
+      i   = 0;
+
+  (function loop() {
+    var element = document.querySelector(els[i]);
+    element.classList.add('animated');
+
+    if (++i < els.length) {
+      setTimeout(loop, 350);
+    }
+  })();
+}
