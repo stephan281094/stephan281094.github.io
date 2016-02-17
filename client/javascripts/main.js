70f12d58f20068efe6b74d9c7e04a2cb5f7d6f69
Stephan de Vries <stephan281094@users.noreply.github.com>
2016-02-17 08:55:57 +0100

    Fix code styling conform to 'standard'

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -1,28 +1,28 @@
-window.fitText(document.querySelector('h1'));
-window.fitText(document.querySelector('span'), 2);
-document.addEventListener('touchstart', function(){}, true);
-
-window.onload = function() {
-  app.fadeElementsIn();
-}
-
 var app = {
-  fadeElementsIn: function() {
+  fadeElementsIn: function () {
     var els = ['h1', 'span', '.contact'],
-        i   = 0;
+        i   = 0
 
-    (function loop() {
-      var element = document.querySelector(els[i]);
-      element.classList.add('animated');
-      element.classList.add('fadeInUp');
+    (function loop () {
+      var element = document.querySelector(els[i])
+      element.classList.add('animated')
+      element.classList.add('fadeInUp')
 
       if (++i < els.length) {
-        setTimeout(loop, 350);
+        setTimeout(loop, 350)
       }
-    })();
+    })()
   },
 
-  contact: function() {
-    document.location.href = 'mailto:stephan281094@gmail.com';
+  contact: function () {
+    document.location.href = 'mailto:stephan281094@gmail.com'
   }
+}
+
+window.onload = function () {
+  window.fitText(document.querySelector('h1'))
+  window.fitText(document.querySelector('span'), 2)
+  document.addEventListener('touchstart', function () {}, true)
+  
+  app.fadeElementsIn()
 }
